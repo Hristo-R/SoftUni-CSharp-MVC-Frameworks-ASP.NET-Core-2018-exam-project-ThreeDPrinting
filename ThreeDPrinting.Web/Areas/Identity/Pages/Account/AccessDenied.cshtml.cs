731270d5f896ceb7b2e6e7c951de49eeb4ff8963
Hristo-R <hristo.razpopov@abv.bg>
2018-08-16 13:43:31 +0300

    Refactoring code

---
+++ ThreeDPrinting.Web/Areas/Identity/Pages/Account/AccessDenied.cshtml.cs
@@ -0,0 +1,12 @@
+﻿namespace ThreeDPrinting.Web.Areas.Identity.Pages.Account
+{
+    using Microsoft.AspNetCore.Mvc.RazorPages;
+
+    public class AccessDeniedModel : PageModel
+    {
+        public void OnGet()
+        {
+
+        }
+    }
+}
---
+++ ThreeDPrinting.Web/Areas/Identity/Pages/Error.cshtml.cs
@@ -0,0 +1,21 @@
+﻿namespace ThreeDPrinting.Web.Areas.Identity.Pages
+{
+    using System.Diagnostics;
+    using Microsoft.AspNetCore.Mvc;
+    using Microsoft.AspNetCore.Mvc.RazorPages;
+    using Microsoft.AspNetCore.Authorization;
+
+    [AllowAnonymous]
+    public class ErrorModel : PageModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public void OnGet()
+        {
+            RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier;
+        }
+    }
+}
