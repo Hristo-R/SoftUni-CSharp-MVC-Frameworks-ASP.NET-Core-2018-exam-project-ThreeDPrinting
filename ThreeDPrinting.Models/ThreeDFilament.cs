5afc27de9bd15d21120a97eff6266602ee648f5d
Hristo-R <hristo.razpopov@abv.bg>
2018-08-21 17:29:39 +0300

    Added Models

---
+++ ThreeDPrinting.Models/ThreeDFilament.cs
@@ -0,0 +1,31 @@
+﻿namespace ThreeDPrinting.Models
+{
+    using System.ComponentModel.DataAnnotations;
+    using ThreeDPrinting.Web.Models;
+
+    public class ThreeDFilament
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [MaxLength(100)]
+        public string Make { get; set; }
+
+        [Required]
+        [MaxLength(100)]
+        public string Color { get; set; }
+
+        [Required]
+        [Range(0, double.MaxValue)]
+        public decimal Price { get; set; }
+
+        [Required]
+        [MinLength(10)]
+        [MaxLength(2000)]
+        public string ImageUrl { get; set; }
+
+        public string UserId { get; set; }
+
+        public User User { get; set; }
+    }
+}
---
+++ ThreeDPrinting.Models/ThreeDPrinter.cs
@@ -0,0 +1,39 @@
+﻿namespace ThreeDPrinting.Models
+{
+    using System.ComponentModel.DataAnnotations;
+    using ThreeDPrinting.Web.Models;
+
+    public class ThreeDPrinter
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [MaxLength(100)]
+        public string Make { get; set; }
+
+        [Required]
+        [MaxLength(100)]
+        public string Model { get; set; }
+
+        [Required]
+        [Range(0, double.MaxValue)]
+        public decimal Price { get; set; }
+
+        [Required]
+        [MaxLength(50)]
+        public string BuildVolume { get; set; }
+
+        [Required]
+        [MaxLength(6000)]
+        public string Description { get; set; }
+
+        [Required]
+        [MinLength(10)]
+        [MaxLength(2000)]
+        public string ImageUrl { get; set; }
+
+        public string UserId { get; set; }
+
+        public User User { get; set; }
+    }
+}
