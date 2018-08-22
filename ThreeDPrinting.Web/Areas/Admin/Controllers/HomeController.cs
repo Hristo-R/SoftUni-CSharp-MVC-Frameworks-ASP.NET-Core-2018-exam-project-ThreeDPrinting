1a90665f7df029c765e709db8aa39b4841676f30
Hristo-R <hristo.razpopov@abv.bg>
2018-08-22 17:31:10 +0300

    Added Area Admin

---
+++ ThreeDPrinting.Web/Areas/Admin/Controllers/HomeController.cs
@@ -0,0 +1,15 @@
+﻿namespace ThreeDPrinting.Web.Areas.Admin.Controllers
+{
+    using Microsoft.AspNetCore.Authorization;
+    using Microsoft.AspNetCore.Mvc;
+
+    [Area("Admin")]
+    [Authorize(Roles = "Administrator")]
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
--- ThreeDPrinting.Web/Startup.cs
+++ ThreeDPrinting.Web/Startup.cs
@@ -88,6 +88,14 @@
             app.UseMvc(routes =>
             {
                 routes.MapRoute(
+                    name: "areas",
+                    template: "{area:exists}/{controller=Home}/{action=Index}/{id?}"
+                );
+            });
+
+            app.UseMvc(routes =>
+            {
+                routes.MapRoute(
                     name: "default",
                     template: "{controller=Home}/{action=Index}/{id?}");
             });
